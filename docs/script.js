c028651cd40e607cdb94d371d12d203bb305f56a
Rey <reudysfernando@gmail.com>
2024-09-22 13:02:51 -0400

    Cambios minimos

--- docs/script.js
+++ docs/script.js
@@ -1,15 +1,28 @@
-let modeActive = false
-function SwitchMode () {
+let modeActive = false;
+function SwitchMode() {
     var modes = document.getElementById("body");
-    var modeslogo = document.getElementById("boton-flotante")
+    var modeslogo = document.getElementById("boton-flotante");
+    var texto = document.getElementsByClassName("text-card");
 
     if (modeActive == false) {
         modes.style.backgroundColor = "#000000";
         modeslogo.style.backgroundImage = "url('https://reudys.github.io/ProyectoWeb/public/nigth-mode.png')";
-        modeActive = true
+
+        // Recorrer la colección de elementos y cambiar el color
+        for (var i = 0; i < texto.length; i++) {
+            texto[i].style.color = "#ffffff";
+        }
+
+        modeActive = true;
     } else {
         modes.style.backgroundColor = "#ffffff";
         modeslogo.style.backgroundImage = "url('https://reudys.github.io/ProyectoWeb/public/main-mode.png')";
-        modeActive = false
+
+        // Recorrer la colección de elementos y cambiar el color
+        for (var i = 0; i < texto.length; i++) {
+            texto[i].style.color = "#000000";
+        }
+
+        modeActive = false;
     }
 }
