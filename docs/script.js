859c45d38e4e68267b5ae88c929ea1d0bb4cc8b7
Rey <reudysfernando@gmail.com>
2024-09-27 07:52:45 -0400

    Prueba de archivos multiuso

--- docs/script.js
+++ docs/script.js
@@ -26,3 +26,7 @@ function SwitchMode() {
         modeActive = false;
     }
 }
+
+fetch('header.html')
+.then(response => response.text())
+.then(data => {document.getElementById('header').innerHTML = data;});
